995545277e35cd86d5c0d06f392b7a44f6d0ad8b
Zayn Zaidi <112359225+zrzaidi@users.noreply.github.com>
2025-03-31 03:00:43 -0400

    Update ArrayUtils.java

--- src/main/java/ArrayUtils.java
+++ src/main/java/ArrayUtils.java
@@ -35,4 +35,12 @@ public class ArrayUtils {
         }
         return count;
     }
+
+     public static int countOf(int[] x, int target) {
+    int count = 0;
+    for (int a: x) {
+        count = a == target ? count + 1 : count;
+    }
+    return count;
+}
 }
